697da409cff467543efe843829979658b788f9bc
Nicole Coppa <coppa.nicole@gmail.com>
2012-12-18 14:15:19 -0700

    getting started

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Babydollweddings::Application.config.secret_token = 'e2479d943204f33c882b959939e5cb9ad0e684ed608d3ae58275ffdef37280d057a30d443f275ef4eaaf0047d9c99f707738e6b727170f04077a7b65acee2484'
